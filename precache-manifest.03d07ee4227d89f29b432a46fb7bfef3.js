24527716898cc5f6cc7be02f7c1d496bc51556fb
BhavYea <43646734+BhavYea@users.noreply.github.com>
2020-04-21 22:47:35 +0530

    Updates

---
+++ precache-manifest.03d07ee4227d89f29b432a46fb7bfef3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426fb8f316c1145d792ecb0bcdef2159",
+    "url": "/snumato-dbms/index.html"
+  },
+  {
+    "revision": "290cce5b81448c3cbcbb",
+    "url": "/snumato-dbms/static/css/main.ca41bd40.chunk.css"
+  },
+  {
+    "revision": "6a19b0dd4af5f064a8a1",
+    "url": "/snumato-dbms/static/js/2.f9fe3941.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/snumato-dbms/static/js/2.f9fe3941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "290cce5b81448c3cbcbb",
+    "url": "/snumato-dbms/static/js/main.79424077.chunk.js"
+  },
+  {
+    "revision": "3b6136dd7b1db5f08695",
+    "url": "/snumato-dbms/static/js/runtime-main.887f5227.js"
+  },
+  {
+    "revision": "af50969144b1fe449c65cba4270cb592",
+    "url": "/snumato-dbms/static/media/3007281_black-01.af509691.jpeg"
+  }
+]);
