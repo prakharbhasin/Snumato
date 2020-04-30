2a6f364c95f4de36c3a9f3f56b5af01682cfd2d6
BhavYea <43646734+BhavYea@users.noreply.github.com>
2020-04-30 20:06:04 +0530

    signup try

--- src/Components/Views/signup.js
+++ src/Components/Views/signup.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Axios from "axios";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         SNUMATO
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -29,18 +30,18 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    backgroundColor:"black",
-    opacity:"0.9"
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "black",
+    opacity: "0.9",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
   submit: {
@@ -71,26 +72,55 @@ const useStyles = makeStyles((theme) => ({
         color: "white",
       },
     },
-  }
+  },
 }));
 
 export default function SignUp() {
   const classes = useStyles();
 
+  const [fname, setfname] = useState(null);
+  const [lname, setlname] = useState(null);
+  const [email, setemail] = useState(null);
+  const [pass, setpass] = useState(null);
+  const [mob, setmob] = useState(null);
+  const credentials = {
+    email: email,
+    password: pass,
+    first_name: fname,
+    last_name: lname,
+    mobile_num: mob,
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(JSON.stringify(credentials));
+    Axios.post(
+      "http://127.0.0.1:8000/user/register",
+      JSON.stringify(credentials)
+    )
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error.message));
+  };
+
   return (
-    <Container style={{background:"black",opacity:"0.9"}}component="main" maxWidth="xs">
+    <Container
+      style={{ background: "black", opacity: "0.9" }}
+      component="main"
+      maxWidth="xs"
+    >
       <CssBaseline />
       <div className={classes.paper}>
         {/*<Avatar className={classes.avatar}>
           <LockOutlinedIcon />
   </Avatar>*/}
-        <Typography component="h1" variant="h5" style={{color:"#EFF"}}>
+        <Typography component="h1" variant="h5" style={{ color: "#EFF" }}>
           Sign up
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
+                onInput={(e) => setfname(e.target.value)}
                 autoComplete="fname"
                 name="firstName"
                 variant="outlined"
@@ -105,6 +135,7 @@ export default function SignUp() {
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
+                onInput={(e) => setlname(e.target.value)}
                 variant="outlined"
                 required
                 fullWidth
@@ -118,6 +149,7 @@ export default function SignUp() {
             </Grid>
             <Grid item xs={12}>
               <TextField
+                onInput={(e) => setemail(e.target.value)}
                 variant="outlined"
                 required
                 fullWidth
@@ -131,6 +163,7 @@ export default function SignUp() {
             </Grid>
             <Grid item xs={12}>
               <TextField
+                onInput={(e) => setpass(e.target.value)}
                 variant="outlined"
                 required
                 fullWidth
@@ -139,6 +172,20 @@ export default function SignUp() {
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                color="secondary"
+                className={classes.textfield}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onInput={(e) => setmob(e.target.value)}
+                variant="outlined"
+                required
+                fullWidth
+                name="mob"
+                label="Mobile Number"
+                id="mob"
+                autoComplete="mobile no"
                 color="secondary"
                 className={classes.textfield}
               />
@@ -161,7 +208,11 @@ export default function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="/snumato-dbms/login_page" variant="body2" color="secondary">
+              <Link
+                href="/snumato-dbms/login_page"
+                variant="body2"
+                color="secondary"
+              >
                 Already have an account? Sign in
               </Link>
             </Grid>
